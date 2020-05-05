b38b8041bbabb3847391003e5023332334463e04
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-05-05 17:36:02 +0100

    Added failing feature test

---
+++ spec/features/add_bookmarks_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require 'pg'
+
+feature 'Adding bookmarks' do
+  scenario 'adds url to bookmarks table' do
+    visit('/bookmarks/add')
+    fill_in :url, with: 'http://www.bbc.com'
+
+    expect(page).to have_content "http://www.bbc.com"
+  end
+end
