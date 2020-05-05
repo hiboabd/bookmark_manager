0af5f78b9a7b24ab305b18837b160202906b43a6
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-05-05 17:21:32 +0100

    Test commit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require_relative './setup_test_database'
 
 ENV['ENVIRONMENT'] = 'test'
-# ENV['RACK_ENV'] = 'test'
+
 RSpec.configure do |config|
   config.before(:each) do
     setup_test_database
